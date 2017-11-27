b5a7986c1db58d531f04242a24bd366afa2dff5e
IFoxH <oldsuw@gmail.com>
2017-11-27 18:04:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ onload = function(){
   var wvpMatrix = m.identity(m.create());
 
   // ビュー×プロジェクション座標変換行列
-  m.lookAt([0.0, 0.0, 10.0], [0, 0, 0], [0, 1, 0], vMatrix);// カメラ位置、注視点、上方向
+  m.lookAt([5.0, 0.0, 10.0], [0, 0, 0], [0, 1, 0], vMatrix);// カメラ位置、注視点、上方向
   m.perspective(45, c.width / c.height, 0.1, 300, pMatrix);// 画角 アスペクト比,近クリップ面,遠方クリップ面
   m.multiply(pMatrix, vMatrix, vpMatrix);
 
